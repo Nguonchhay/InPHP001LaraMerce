a4a4842e7b9b49d684e6eb4ea3405afd8c1d0926
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-06-24 20:01:47 +0700

    [TASK] Backend middleware role

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,25 @@ Route::get('/contact-us', [App\Http\Controllers\PageController::class, 'contact'
 
 // Backend routes
 Route::group([
-    'middleware' => 'auth'
+    'middleware' => 'auth',
+    'prefix' => 'backend'
 ], function() {
-    Route::get('/backend/categories', [App\Http\Controllers\Backend\CategoryController::class, 'index'])->name('backend.categories.index');
-    Route::get('/backend/categories/create', [App\Http\Controllers\Backend\CategoryController::class, 'create'])->name('backend.categories.create');
-    Route::post('/backend/categories', [App\Http\Controllers\Backend\CategoryController::class, 'store'])->name('backend.categories.store');
-    Route::get('/backend/categories/{category}/edit', [App\Http\Controllers\Backend\CategoryController::class, 'edit'])->name('backend.categories.edit');
-    Route::put('/backend/categories/{category}', [App\Http\Controllers\Backend\CategoryController::class, 'update'])->name('backend.categories.update');
-    Route::delete('/backend/categories/{category}', [App\Http\Controllers\Backend\CategoryController::class, 'destroy'])->name('backend.categories.destroy');
+
+    // Admin role
+    Route::group([
+        'middleware' => \App\Http\Middleware\BackendRoleMiddleware::class
+    ], function() {
+        Route::get('/categories', [App\Http\Controllers\Backend\CategoryController::class, 'index'])->name('backend.categories.index');
+        Route::get('/categories/create', [App\Http\Controllers\Backend\CategoryController::class, 'create'])->name('backend.categories.create');
+        Route::post('/categories', [App\Http\Controllers\Backend\CategoryController::class, 'store'])->name('backend.categories.store');
+        Route::get('/categories/{category}/edit', [App\Http\Controllers\Backend\CategoryController::class, 'edit'])->name('backend.categories.edit');
+        Route::put('/categories/{category}', [App\Http\Controllers\Backend\CategoryController::class, 'update'])->name('backend.categories.update');
+        Route::delete('/categories/{category}', [App\Http\Controllers\Backend\CategoryController::class, 'destroy'])->name('backend.categories.destroy');    
+    });
     
+   
     Route::resource(
-        'backend/products',
+        'products',
         App\Http\Controllers\Backend\ProductController::class,
         [ 'as' => 'backend']
     );
